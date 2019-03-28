d470bfddae274352b6bf3fde064de381d8fd0f4d
xjplus <xxjplus@github.com>
2019-03-28 21:26:20 +0800

    init

---
+++ app/src/main/java/io/github/xuqplus/yigetestapp/MainActivity.java
@@ -0,0 +1,42 @@
+package io.github.xuqplus.yigetestapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button0 = findViewById(R.id.button_0);
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onclick(null);
+            }
+        });
+
+        Button button1 = findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onclick("点击了, 1号按钮");
+            }
+        });
+    }
+
+    public String onclick(String toastContent) {
+        String r = null != toastContent ? toastContent : getToastContent();
+        Toast.makeText(this, r, Toast.LENGTH_SHORT).show();
+        return r;
+    }
+
+    public String getToastContent() {
+        return "默认的ToastContent";
+    }
+}
